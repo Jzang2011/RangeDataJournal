a602d1c5580387108db30e16fd3a45175205f48c
Jeremy Zang <zang.jeremy@gmail.com>
2020-02-21 22:25:38 -0500

    Adding codebase to github. My intentions are to only include source files so that the project can be cloned to be developed by others.

---
+++ RangeDataJournalApp/src/main/java/com/zang/jeremy/Main.java
@@ -0,0 +1,31 @@
+package com.zang.jeremy;
+
+import com.zang.jeremy.views.PrimaryView;
+import com.zang.jeremy.views.SecondaryView;
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends MobileApplication {
+
+    public static final String PRIMARY_VIEW = HOME_VIEW;
+    public static final String SECONDARY_VIEW = "Secondary View";
+
+    @Override
+    public void init() {
+        addViewFactory(PRIMARY_VIEW, () -> new PrimaryView().getView());
+        addViewFactory(SECONDARY_VIEW, () -> new SecondaryView().getView());
+
+        DrawerManager.buildDrawer(this);
+    }
+
+    @Override
+    public void postInit(Scene scene) {
+        Swatch.BLUE.assignTo(scene);
+
+        scene.getStylesheets().add(Main.class.getResource("style.css").toExternalForm());
+        ((Stage) scene.getWindow()).getIcons().add(new Image(Main.class.getResourceAsStream("/icon.png")));
+    }
+}
